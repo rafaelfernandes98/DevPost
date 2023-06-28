743d43458ad40d17d8f97b367c2b331cb6007818
rafaelfernandes98 <rafael130140@gmail.com>
2023-06-27 21:28:48 -0300

    Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,25 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather'
+import { Container, ButtonPost } from './styles';
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
+
 
 export function Home() {
+  const navigation = useNavigation()
   return (
-    <View>
+    <Container>
       <Text>Home</Text>
-    </View>
+      <ButtonPost
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate('NewPost' as never)}
+      >
+        <Feather
+          name='edit-2'
+          color='#fff'
+          size={25}
+        />
+      </ButtonPost>
+    </Container>
   );
 }
---
+++ src/pages/Home/styles.tsx
@@ -0,0 +1,19 @@
+import { styled } from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #36393f;
+`
+
+export const ButtonPost = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 5%;
+  right: 6%;
+  width: 60px;
+  height: 60px;
+  background-color: #202225;
+  border-radius: 35px;
+  justify-content: center;
+  align-items: center;
+  z-index: 99;
+`
