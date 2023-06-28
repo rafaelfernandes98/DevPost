d4ead666b5341b942b4bf897e9724a59db595cd4
rafaelfernandes98 <rafael130140@gmail.com>
2023-06-27 21:28:54 -0300

    app routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,17 +1,92 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Home } from '../pages/Home';
 import { Search } from '../pages/Search';
 import { Profile } from '../pages/Profile';
+import Feather from 'react-native-vector-icons/Feather'
+import { NewPost } from '../pages/NewPost';
+import { PostsUser } from '../pages/PostsUser';
 
 const Tab  = createBottomTabNavigator()
+const Stack = createNativeStackNavigator()
+
+function StackRoutes(){
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='home'
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name='NewPost'
+        component={NewPost}
+        options={{
+          title: 'Novo Post',
+          headerTintColor: '#fff',
+          headerStyle: {
+            backgroundColor: '#36393f'
+          }
+        }}
+      />
+      <Stack.Screen
+        name='PostsUser'
+        component={PostsUser}
+        options={{
+          headerTintColor: '#fff',
+          headerStyle: {
+            backgroundColor: '#36393f'
+          }
+        }}
+      />
+    </Stack.Navigator>
+  )
+}
 
 export function AppRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Home' component={Home} />
-      <Tab.Screen name='Search' component={Search} />
-      <Tab.Screen name='Profile' component={Profile} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: '#202225',
+          borderTopWidth: 0
+        }
+      }}
+    >
+      <Tab.Screen
+        name='HomeTab'
+        component={StackRoutes}
+        options={{
+          tabBarIcon: ({color, size})=>{
+            return <Feather name='home' color={color} size={size}/>
+          }
+        }}
+      />
+      <Tab.Screen
+        name='Search'
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name='search' color={color} size={size} />
+          }
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name='user' color={color} size={size} />
+          }
+        }}
+      />
     </Tab.Navigator>
   )
 }
