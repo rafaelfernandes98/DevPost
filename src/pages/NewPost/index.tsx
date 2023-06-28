06ffa89addd64ffa676cf8b9582bef0e24abab54
rafaelfernandes98 <rafael130140@gmail.com>
2023-06-27 21:28:40 -0300

    New post

--- src/pages/NewPost/index.tsx
+++ src/pages/NewPost/index.tsx
@@ -1,12 +1,38 @@
-import * as React from 'react';
 
 import { Text, View } from 'react-native';
+import { Button, ButtonText, Container, Input } from './styles';
+import { useLayoutEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 
 export function NewPost() {
+  const navigation = useNavigation()
+  const [post, setPost] = useState('');
+
+  useLayoutEffect(()=>{
+
+    const options = navigation.setOptions({
+      headerRight: ()=> (
+        <Button>
+          <ButtonText>Compartilhar</ButtonText>
+        </Button>
+      )
+    })
+
+  }, [navigation, post])
+
   return (
-    <View>
-      <Text>NewPost</Text>
-    </View>
+    <Container>
+      <Input
+        placeholder='O que está acontecendo?'
+        value={post}
+        onChangeText={(t)=> setPost(t)}
+        autoCorrect={false}
+        multiline={true}
+        placeholderTextColor='#ddd'
+        maxLength={300}
+      />
+
+    </Container>
   );
 }
